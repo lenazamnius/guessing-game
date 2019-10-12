42cd23bb0c8341418a5587d0b836abba1bf5ece1
Lena Zamnius <lena.zamnius@gmail.com>
2019-10-12 18:44:50 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.medium = 0;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.medium = Math.ceil((this.max + this.min) / 2);
+      return this.medium;
     }
 
     lower() {
-
+      this.max = this.medium;
     }
 
     greater() {
-
+      this.min = this.medium;
     }
 }
 
